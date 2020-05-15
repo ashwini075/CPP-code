de016bae5b8e877e7a0bd9bf7b49b8e0b50ca7bc
ashwini075 <ashwinigaikar075@gmail.com>
2020-05-15 09:40:49 +0530

    hard coded value taken for finding compound intrest

---
+++ CompoundIntrest.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+	int priciple,rate,n,duration,CI;          
+	double amount=principle * pow(1+(rate/n),(n*duration));
+	 amount=5000 * pow(1+(0.10/10),(10*10));
+	CI=amount-principle;
+	cout<<CI;
+	return 0;
+}
